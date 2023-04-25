06421341d4bbbd103df28e63426066b7107888c9
Ilan Velter <ivelter0@gmail.com>
2023-04-25 10:02:32 +0200

    It works !

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import dotenv
 import tweepy
 from os import listdir
+from os import environ
 from random import choice
 
 def getrandomimg() -> str:
-    return './img/' + choice(listdir('./img'))
+    return 'img/' + choice(listdir('./img'))
 
-consumerkey = dotenv.get_key("CONSUMER_KEY")
-consumersecret = dotenv.get_key("CONSUMER_SECRET")
-tokenkey = dotenv.get_key("TOKENKEY")
-tokensecret = dotenv.get_key("TOKENSECRET")
+dotenv.load_dotenv(".env")
+consumerkey = environ.get("CONSUMER_KEY")
+consumersecret = environ.get("CONSUMER_SECRET")
+tokenkey = environ.get("TOKENKEY")
+tokensecret = environ.get("TOKENSECRET")
 
 auth = tweepy.OAuthHandler(consumer_key=consumerkey, consumer_secret=consumersecret)
 auth.set_access_token(key=tokenkey, secret=tokensecret)
@@ -18,3 +20,4 @@ api = tweepy.API(auth)
 ret = api.media_upload(getrandomimg())
 
 api.update_status(media_ids=[ret.media_id_string], status="")
+# api.update_status(status="Hello world")
